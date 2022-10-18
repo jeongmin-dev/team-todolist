8d349a0465813e4771a657bd36eb52854b180d7b
hansejun <tpwnszja2@gmail.com>
2022-10-18 15:13:59 +0900

    update timeFn

--- src/components/util/timeCheck.js
+++ src/components/util/timeCheck.js
@@ -5,7 +5,7 @@ function timeCheck(prevTime = 1665846659220) {
   else if (1 < gap && gap < 60) return `${gap | 0}분 전`;
   else if (60 <= gap && gap < 1440) return `${parseInt(gap / 60)}시간 전`;
   else if (1440 <= gap && gap < 2880) return "하루 전";
-  else if (2880 < -gap && gap < 43200) return `${parseInt(gap / 60 / 30)}일 전`;
+  else if (2880 < gap && gap < 43200) return `${parseInt(gap / 60 / 30)}일 전`;
   return gap;
 }
 
