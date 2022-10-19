ae64c1d37f4e486c128aa5d0dc92ffeb59cba93f
hansejun <tpwnszja2@gmail.com>
2022-10-20 02:58:59 +0900

    Update List.jsx

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,5 +1,5 @@
 import { AnimatePresence } from "framer-motion";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import { toggleIsLoading } from "../../redux/modules/animation";
